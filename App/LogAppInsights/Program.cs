dcd02fe98208f1adcf5d2edb489d723076726b66
tallesvaliatti <talles.dsv@gmail.com>
2021-11-19 21:53:06 -0300

    update

--- App/LogAppInsights/Program.cs
+++ App/LogAppInsights/Program.cs
@@ -12,11 +12,10 @@ builder.Services.AddApplicationInsightsTelemetry();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseAuthorization();
 
